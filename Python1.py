12518b7b5f884e3257ab4260d8fcab95335eb0ea
Mansi Mittal <mansimittal744@gmail.com>
2026-03-17 12:32:16 +0530

    if else assignment

--- Python1.py
+++ Python1.py
@@ -1,1 +1,722 @@
-print("My first program")
+#Q:1 Write a Python program to check if a number is positive. 
+a=int(input("enter no. :"))
+if a>0:
+    print("no.is positive",a)
+
+#Q:2 Print "Eligible to vote" if age is 18 or above.
+
+age=int(input("enter age :"))
+if age>=18:
+    print("Eligible to vote")
+
+#Q:3. Check if a number is divisible by 7. 
+
+num=int(input("enter no. :"))
+if num%7==0:
+     print("number is divisible by 7")
+
+#Q-4. Print "Pass" if marks are greater than 40.
+marks=int(input("enter your marks:"))
+if marks>40:
+    print("Pass")
+    
+#Q-5. Check if a number is greater than 100.
+
+num=int(input("enter no. :"))
+if num>100:
+     print(" number is greater than 100")
+
+#Q-6. Display a message if temperature exceeds 45°C.
+
+temp=int(input("enter temp :"))
+if temp>45:
+    print("temperature exceeds 45°C")
+#Q-7. Check if a string length is more than 8 characters.
+
+name=input("enter your info :")
+if len(name)>8:
+    print("your info is more than 8 characters")
+
+#Q-8. Print "Logged In" if password matches "admin123"
+
+password =input("enter your password :")
+if password =="admin123":
+    print("Logged In")
+
+#Q-9. Check if a number is a multiple of 10.
+    
+num=int(input("enter no. :"))
+if num%10==0:
+     print("number is multiple of 10")
+     
+#Q-10. Print a warning if balance is below minimum limit.
+
+balance=int(input("enter balance :"))
+if balance<1000:
+    print("your balance is low")
+
+#Q-11. Check whether a number is even or odd.
+num=int(input("enter no. :"))
+if num%2==0:
+     print("number is even")
+else :
+    print("number is odd")
+
+#Q-12. Find the largest of two numbers.
+
+a=int(input("enter first no. :"))
+b=int(input("enter second no. :"))
+if a>b:
+    print("a is greater")
+else :
+    print("b is greater")
+
+#Q-13.Check whether a person is eligible for driving license.    
+
+age=int(input("enter age:"))
+if age>=18:
+    print("eligible for driving license")
+else :
+    print("not eligible for driving license")
+
+#Q-14. Print "Pass" or "Fail" based on marks.
+
+marks=int(input("enter your marks:"))
+if marks>=40:
+    print("Pass")
+else:
+    print("fail")
+
+#Q-15. Check whether a number is positive or negative. 
+
+num=int(input("enter a number:"))
+if num>0:
+    print("number is positive",num)
+else :
+    print("number is negative",num)
+
+#Q-16. Check whether a character is a vowel or consonant.
+
+ch = input("Enter A CHaracter : ")
+if ch in "AEIOUaeiou":
+    print("Vowel")
+else:
+    print("Consonant")
+#Q-17. Check if a year is leap or not.
+
+year=int(input("enter year :"))
+if (year%400==0) or (year%4==0 and year%100 !=0):
+    print("Leap Year")
+else:
+    print("Not Leap Year")
+
+#Q-18. Print "Valid Password" or "Invalid Password"
+    
+password =input("enter your password :")
+if password =="admin123":
+    print("Valid Password")
+else :
+     print("InValid Password")
+
+#Q-19. Determine whether salary is taxable or not. 
+
+salary=int(input("enter your salary :"))
+if salary == 50000:
+    print("salary is taxable")
+else :
+    print("salary is not taxable")
+
+#Q-20. Check whether a number is greater than 50 or not.
+
+a=int(input("enter no. :"))
+if a>50:
+    print("no.is greater than 50",a)
+else:
+    print("no.is not greater than 50",a)
+
+
+#Q-21. Find the largest of three numbers.
+
+a=int(input("enter a"))
+b=int(input("enter b"))
+c=int(input("enter c"))
+if a>b:
+    if a>c:
+        print("greater value ",a)
+    else:
+        print("greater value ",c)
+else:
+    if b>c:
+        print("greater value ",b)
+    else:
+        print("greater value ",c)
+
+#Q-22. Check whether a number is positive, negative, or zero
+
+num=int(input("enter a number: "))
+if num>=0:
+    if num==0:
+        print("number is zero")
+    else:
+        print("number is positive")
+else:
+    print("number is negative")
+#Q=23. Assign grades: ● A → marks ≥ 90 ● B → marks ≥ 75 ● C → marks ≥ 60 ● Fail → below 60
+
+if marks>=60:
+    if marks>=75:
+        if marks>=90:
+            print("Grade A")
+        else:
+            print("Grade B")
+    else:
+        print("Grade C")
+else:
+    print("Fail")
+
+#Q=24. Check whether a triangle is equilateral, isosceles, or scalene.
+
+a=int(input("enter side 1: "))
+b=int(input("enter side 2: "))
+c=int(input("enter side 3: "))
+if a==b:
+    if b==c:
+        print("Equilateral Triangle")
+    else:
+        print("Isosceles Triangle")
+else:
+    if a==c or b==c:
+        print("Isosceles Triangle")
+    else:
+        print("Scalene Triangle")
+        
+#Q=25. Check whether a character is uppercase, lowercase, digit, or special character.
+
+ch = input("Enter A Character : ")
+if ch>='0':
+    if ch<='9':
+        print("Digit")
+    else:
+        if ch<='Z':
+            print("Upper Case")
+        else:
+            if ch<='z':
+                print("Lower Case")
+            else:
+                print('Special Character')
+else:
+    print('Special Character')
+
+#Q=26. Calculate electricity bill using slab-wise rates.
+#hint-0–100 units → ₹5/unit ,101–200 units → ₹7/unit ,Above 200 → ₹10/unit
+
+service=250
+units=int(input("enter electricity units: "))
+if units<=100:
+    bill=units*5
+else:
+    if units<=200:
+        bill=units*7
+    else:
+        bill=units*10
+bill=bill+service
+print("Electricity bill =",bill)
+
+#Q=27. Validate login using username and password.
+
+username=input("enter username: ")
+password=input("enter password: ")
+if username=="admin":
+    if password=="1234":
+        print("Login Successful")
+    else:
+        print("Wrong Password")
+else:
+    print("Invalid Username")
+#Q=28. Check student result using marks of 3 subjects.
+
+s1=int(input("enter marks of subject 1: "))
+s2=int(input("enter marks of subject 2: "))
+s3=int(input("enter marks of subject 3: "))
+if s1>=40:
+    if s2>=40:
+        if s3>=40:
+            print("Student Passed")
+        else:
+            print("Fail in subject 3")
+    else:
+        print("Fail in subject 2")
+else:
+    print("Fail in subject 1")
+    
+#Q=29. Find the second largest number among three numbers.
+
+a=int(input("enter first number: "))
+b=int(input("enter second number: "))
+c=int(input("enter third number: "))
+
+if a>b:
+    if a<c:
+        print("Second largest =",a)
+    else:
+        if b>c:
+            print("Second largest =",b)
+        else:
+            print("Second largest =",c)
+else:
+    if b<c:
+        print("Second largest =",b)
+    else:
+        if a>c:
+            print("Second largest =",a)
+        else:
+            print("Second largest =",c)
+            
+#Q=30. Check loan eligibility using age, salary, and credit score. solve this
+
+age=int(input("enter age: "))
+salary=int(input("enter salary: "))
+credit=int(input("enter credit score: "))
+
+if age>=21:
+    if salary>=25000:
+        if credit>=700:
+            print("Loan Approved")
+        else:
+            print("Low Credit Score")
+    else:
+        print("Salary too low")
+else:
+    print("Not eligible due to age")
+
+#31. Print Day Name Using Day Number(1-7)
+day=int(input("enter day number (1-7): "))
+
+if day==1:
+    print("Monday")
+elif day==2:
+    print("Tuesday")
+elif day==3:
+    print("Wednesday")
+elif day==4:
+    print("Thursday")
+elif day==5:
+    print("Friday")
+elif day==6:
+    print("Saturday")
+elif day==7:
+    print("Sunday")
+else:
+    print("Invalid day number")
+    
+#32. Print Month Name Using Month Number
+month=int(input("enter month number (1-12): "))
+
+if month==1:
+    print("January")
+elif month==2:
+    print("February")
+elif month==3:
+    print("March")
+elif month==4:
+    print("April")
+elif month==5:
+    print("May")
+elif month==6:
+    print("June")
+elif month==7:
+    print("July")
+elif month==8:
+    print("August")
+elif month==9:
+    print("September")
+elif month==10:
+    print("October")
+elif month==11:
+    print("November")
+elif month==12:
+    print("December")
+else:
+    print("Invalid month number")
+    
+#33. display Grade Based on Percentage
+per=float(input("enter percentage: "))
+
+if per>=90:
+    print("Grade A")
+elif per>=75:
+    print("Grade B")
+elif per>=60:
+    print("Grade C")
+elif per>=40:
+    print("Grade D")
+else:
+    print("Fail")
+    
+#34. . Display bonus percentage based on experience years.
+exp=int(input("enter years of experience: "))
+
+if exp>=10:
+    print("Bonus = 20%")
+elif exp>=5:
+    print("Bonus = 10%")
+elif exp>=2:
+    print("Bonus = 5%")
+else:
+    print("No Bonus")
+    
+#35. Traffic Signal Meaning
+signal=input("enter traffic signal color: ")
+
+if signal=="red":
+    print("Stop")
+elif signal=="yellow":
+    print("Wait")
+elif signal=="green":
+    print("Go")
+else:
+    print("Invalid signal")
+    
+#36. Categorize temperature as Cold / Warm / Hot. 
+temp=int(input("enter temperature: "))
+
+if temp<15:
+    print("Cold")
+elif temp<30:
+    print("Warm")
+else:
+    print("Hot")
+    
+#37.  Categorize employee based on salary range. 
+salary=int(input("enter salary: "))
+
+if salary<20000:
+    print("Low Salary")
+elif salary<50000:
+    print("Medium Salary")
+elif salary<100000:
+    print("High Salary")
+else:
+    print("Very High Salary")
+    
+#38.Print discount percentage based on purchase amount. 
+amount=int(input("enter purchase amount: "))
+
+if amount>=5000:
+    print("Discount = 20%")
+elif amount>=3000:
+    print("Discount = 15%")
+elif amount>=1000:
+    print("Discount = 10%")
+else:
+    print("No Discount")
+    
+#39.Identify number type: single-digit / double-digit / multi-digit.
+
+num=int(input("enter a number: "))
+
+if num>=0 and num<=9:
+    print("Single digit number")
+elif num>=10 and num<=99:
+    print("Double digit number")
+else:
+    print("Multi digit number")
+    
+#40. Assign performance rating: Poor / Average / Good / Excellent. 
+score=int(input("enter performance score: "))
+
+if score>=90:
+    print("Excellent")
+elif score>=70:
+    print("Good")
+elif score>=50:
+    print("Average")
+else:
+    print("Poor")
+
+#41. Check whether a number is divisible by 5 and 11.
+num=int(input("enter a num"))
+if num%5==0 and num%11==0:
+    print("number is divisible by 5 and 11")
+else:
+    print("number is not divisible by 5 and 11")
+    
+#42. Check if a person is eligible for loan: ● age ≥ 21 ● salary ≥ 25,000 ● credit score ≥ 700 
+
+age = int(input("Enter age: "))
+salary = int(input("Enter salary: "))
+credit = int(input("Enter credit score: "))
+
+if age >= 21 and salary >= 25000 and credit >= 700:
+    print("Eligible for loan")
+else:
+    print("Not eligible")
+#43. Validate login using username AND password.
+
+username=input("enter username: ")
+password=input("enter password: ")
+if username=="admin" and password=="1234":
+    print("Login Successful")
+else:
+    print("Invalid Username")
+
+#44. Check student pass condition: ● All subjects ≥ 40 ● Average ≥ 50 
+
+s1=int(input("enter marks of subject 1:"))
+s2=int(input("enter marks of subject 2:"))
+s3=int(input("enter marks of subject 3:"))
+
+average=(s1+s2+s3)/3
+if s1>=40 and s2>=40 and s3>=40 and average>=50 :
+    print("student pass")
+else:
+    print("student failed")
+    
+#45. Check if a number lies between 10 and 100.
+
+num = int(input("Enter number: "))
+
+if num>10 and num<100:
+    print("number lies between 10 and 100")
+else:
+    print("number not in range")
+    
+#46. Check exam eligibility: ● attendance ≥ 75% OR ● medical certificate available 
+
+attendance = int(input("Enter attendance percentage: "))
+medical = input("Medical certificate available (yes/no): ")
+
+if attendance>=75 or medical == "yes":
+    print ("eligible for exam")
+else:
+    print("not eligible")
+      
+#47. Validate a date using conditions.
+
+day = int(input("Enter day: "))
+month = int(input("Enter month: "))
+year = int(input("Enter year: "))
+
+if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
+    leap = True
+else:
+    leap = False
+
+
+if month == 2:
+
+    if leap and (1 <= day <= 29):
+        print("Valid Date")
+
+    elif not leap and (1 <= day <= 28):
+        print("Valid Date")
+
+    else:
+        print("Invalid Date")
+
+elif month in [4,6,9,11]:
+    if day >= 1 and day <= 30:
+        print("Valid Date")
+    else:
+        print("Invalid Date")
+
+elif month in [1,3,5,7,8,10,12]:
+    if day >= 1 and day <= 31:
+        print("Valid Date")
+    else:
+        print("Invalid Date")
+
+else:
+    print("Invalid Month")
+
+#48. Check whether an email format is valid.
+
+email = input("Enter an email id: ")
+if "@" in email and "." in email:
+    print("Valid Email")
+else:
+    print("Invalid Email")
+    
+#49. Determine insurance eligibility using age, health status, and income. 
+
+age = int(input("Enter age: "))
+health = input("Health status (good/bad): ")
+income = int(input("Enter income(per month): "))
+
+if (18 <= age <= 60) and health == "good" and income >= 20000:
+    print("Eligible for insurance")
+else:
+    print("Not eligible")
+#50. Check leap year using complete leap year logic.
+    
+year = int(input("Enter year: "))
+
+if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
+    print("Leap Year")
+else:
+    print("Not a Leap Year")
+
+#51. Write a Python program to calculate income tax using slabs. 
+
+'''hint : Example slabs:
+
+0–250000 → No tax
+
+250000–500000 → 5%
+
+500000–1000000 → 20%
+
+Above 1000000 → 30%'''
+
+income = int(input("enter ypur income :"))
+
+if income<=250000:
+    tax=0
+elif income<=500000:
+    tax = income*0.05
+elif income <=1000000:
+    tax= income*0.20
+else:
+    tax= income*0.30
+
+print("Tax=",tax)
+#52. Create an ATM withdrawal program with balance checks. 
+
+balance = 10000
+amount = int(input("enter withdrawal amount:"))
+
+if amount<=balance :
+    balance=balance - amount
+    print("transaction successful")
+    print("remaining balance :",balance)
+else :
+    print("insufficient balance")
+     
+#53. Check promotion eligibility using experience and performance. 
+
+experience = int(input("Enter years of experience: "))
+performance = int(input("Enter performance rating (1-10): "))
+
+if experience >= 6 and performance >= 8:
+    print("Eligible for promotion")
+else:
+    print("Not eligible")
+#54. Implement a grading system using nested if–else.
+
+marks = int(input("Enter marks: "))
+if marks>=60:
+    if marks>=75:
+        if marks>=90:
+            print("Grade A")
+        else:
+            print("Grade B")
+    else:
+        print("Grade C")
+else:
+    print("Fail")
+   
+#55. Validate strong password using multiple conditions.
+
+password = input("Enter password: ")
+
+for ch in password:
+    if ch >= 'A' and ch <= 'Z':
+        uppercase = True
+    elif ch >= '0' and ch <= '9':
+        digit = True
+    elif ch in "@#$%&_":
+        special = True
+
+if len(password) >= 8 and uppercase and digit and special:
+    print("Strong Password")
+else:
+    print("Wrong password")
+
+#56. Calculate delivery charges based on location and order amount. 
+'''Example logic
+
+Order ≥ 500 → Free delivery
+
+Otherwise
+
+Local → ₹30
+
+Other city → ₹80'''
+
+amount=int(input("enter order amount:"))
+location = input("enter location (local/other):")
+
+if amount>=500:
+    print("Free Delivery")
+else:
+    if location == "location":
+        print("Delivery charge = 30")
+    else:
+        print("Delivery charge = 80")
+        
+#57. Determine online exam qualification.
+
+attendance = int(input("Enter attendance %: "))
+assignment = input("Assignment submitted (yes/no): ")
+
+if attendance >= 75 and assignment == "yes":
+    print("Student is qualified for online exam")
+else:
+    print("Student is not qualified")
+#58. Create movie ticket pricing logic based on age & show time. 
+'''Child (<12) → ₹100
+
+Adult → ₹200
+
+Evening show → ₹50 extra'''
+
+age = int(input("Enter age: "))
+show = input("Show time (morning/evening): ")
+
+if age < 12:
+    price = 300
+else:
+    price = 500
+
+if show == "evening":
+    price = price + 100
+
+print("Ticket price =", price)
+
+#59. Determine bank account type based on balance.
+'''balance ≥ 100000 → Premium
+
+balance ≥ 20000 → Savings
+
+otherwise → Basic'''
+
+balance = int(input("Enter balance: "))
+
+if balance >= 1000000:
+    print("Premium Account")
+elif balance >= 100000:
+    print("Savings Account")
+else:
+    print("Basic Account")
+#60. Create a menu-driven program using if–elif–else.
+
+print("1. Check Even")
+print("2. Check Odd")
+
+choice = int(input("Enter your choice (1,2): "))
+num = int(input("Enter number: "))
+
+if choice == 1:
+    if num % 2 == 0:
+        print("Number is Even")
+    else:
+        print("operator.py")
+
+elif choice == 2:
+    if num % 2 != 0:
+        print("Number is Odd")
+    else:
+        print("not a Odd no.")
+
+else:
+    print("Invalid choice")
