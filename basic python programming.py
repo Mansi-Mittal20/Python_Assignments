6996d4cdbfa98b30e47464853fc0c52ad90527c2
Mansi Mittal <mansimittal744@gmail.com>
2026-03-28 23:19:37 +0530

    basic python programming.py

--- basic python programming.py
+++ basic python programming.py
@@ -1,49 +1,48 @@
-1. Write a program to print "Hello Python" on the screen.
+#1. Write a program to print "Hello Python" on the screen.
 print("hello python")
 
-2. Write a program to take your name as input and print "Hello <name>".
+#2. Write a program to take your name as input and print "Hello <name>".
 name=input("enter your name :")
 print("hello",name)
 
-3.Write a program to take one integer input from the user and print it. 
+#3.Write a program to take one integer input from the user and print it. 
 a=int(input("Enter A Number:"))
 print("your no.",a)
 
-4. Write a program to take two integers as input and print their sum.
+#4. Write a program to take two integers as input and print their sum.
 a=int(input("Enter A Number:"))
 b=int(input("Enter B Number:"))
 sum=a+b
 print("sum:",sum)
 
-5. Write a program to take two integers as input and print their multiplication. 
+#5. Write a program to take two integers as input and print their multiplication. 
 a=int(input("Enter A Number:"))
 b=int(input("Enter B Number:"))
 multipication=a*b
 print("multipication of a&b:",multipication)
 
-6. Write a program to take one float number as input and print it. 
+#6. Write a program to take one float number as input and print it. 
 a=float(input("Enter A Number:"))
 print("your no.",a)
 
-7. Write a program to take two float numbers as input and print their average. 
+#7. Write a program to take two float numbers as input and print their average. 
 a=float(input("Enter A Number:"))
 b=float(input("Enter B Number:"))
 c=(a+b)/2
 print("Addition:",c)
 
-8. Write a program to take length and width (float) from the user and calculate the area of 
+#8. Write a program to take length and width (float) from the user and calculate the area of 
 a rectangle.
 lenth=float(input("Enter length of rectangle:"))
 width=float(input("Enter width of rectangle:"))
 rectanglearea=length*width
 
-9. Write a program to take an integer input and convert it into float, then print it.
+#9. Write a program to take an integer input and convert it into float, then print it.
 a=int(input("Enter A Number:"))
 a=float(a)
 print("your no.",a)
 
-10. Write a program to take price of three items (float) and print the total price."""
-
+#10. Write a program to take price of three items (float) and print the total price."""
 a=float(input("Enter A Number:"))
 b=float(input("Enter B Number:"))
 c=float(input("Enter C Number:"))
